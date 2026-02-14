5f779c44872d602c75029e2f088e6f2c06548b3a
Devlopar <Devlopar123@gmail.com>
2026-02-14 18:30:10 +0530

    remove build from tracking

--- build/CMakeFiles/Snake.dir/src/Board.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Snake.dir/src/Board.cpp.o: \
- /home/pranav/Documents/c++_code/Snake-Console-/src/Board.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/Snake.dir/src/Food.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Snake.dir/src/Food.cpp.o: \
- /home/pranav/Documents/c++_code/Snake-Console-/src/Food.cpp \
- /usr/include/stdc-predef.h
